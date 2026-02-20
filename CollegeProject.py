01baaff10ae6e167a5d7d59ce377b4d9feb0b04a
Anas <anasjahagirdar1234@gmail.com>
2026-02-20 10:18:56 +0530

    added txt files

--- CollegeProject.py
+++ CollegeProject.py
@@ -1,3 +1,5 @@
+import pyodbc
+
 def get_user_input():
     name = input("Enter student name: ").strip()
     subject = input("Enter subject (HR / Finance / Marketing / DS): ").strip()
@@ -19,7 +21,18 @@ def get_user_input():
 
 name, subject, analytics, hostel, food_months, transport = get_user_input()
 
+conn = pyodbc.connect(
+    'DRIVER={SQL Server};'
+    'SERVER=LAPTOP-NUB4LAN9\SQLEXPRESS;'
+    'DATABASE=College_Project_DB;'
+    'Trusted_Connection=yes;'
+)
+cursor = conn.cursor()
 
+<<<<<<< HEAD
+=======
+#########################################################################################################################################
+>>>>>>> 7a689d9 (updated project)
 course_fee = 200000
 
 
@@ -48,6 +61,10 @@ def food_cost(months):
 
 food_fee = food_cost(food_months)
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 7a689d9 (updated project)
 def transport_cost(transport):
     if transport == "semester":
         return 13000
@@ -58,6 +75,10 @@ def transport_cost(transport):
     
 transport_fee = transport_cost(transport)
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 7a689d9 (updated project)
 total_cost = (
     course_fee +
     analytics_fee +
@@ -66,10 +87,20 @@ total_cost = (
     transport_fee
 )
 
+<<<<<<< HEAD
+=======
+
+import os
+
+>>>>>>> 7a689d9 (updated project)
 def save_bill(name,course_fee, analytics_fee, hostel_fee, food_fee, transport_fee, total):
-    with open("annual_bill.txt", "a") as f:
+    filename = f"{name}_bill.txt" 
+    folder = os.path.dirname(os.path.abspath(__file__))
+    filepath = os.path.join(folder, filename)
+
+    with open(filepath, "w") as f:
         f.write("\n========================================================\n")
-        f.write("--- Annual Cost Breakdown ---\n")
+        f.write("--- Annual Cost Of your College is ----\n")
         f.write("Student Name: " + name + "\n")
         f.write("Course Fee: " + str(course_fee) + "\n")
         f.write("Analytics Fee: " + str(analytics_fee) + "\n")
@@ -78,4 +109,16 @@ def save_bill(name,course_fee, analytics_fee, hostel_fee, food_fee, transport_fe
         f.write("Transportation Fee: " + str(transport_fee) + "\n")
         f.write("Total Annual Cost: " + str(total) + "\n")
 
-save_bill(name,course_fee, analytics_fee, hostel_fee, food_fee, transport_fee, total_cost)
+    print("Bill saved:", filepath)
+
+
+cursor.execute("""
+INSERT INTO student_bill
+(name, subject, analytics, hostel, food_months, transport, total_fee)
+VALUES (?, ?, ?, ?, ?, ?, ?)
+""", name, subject, analytics, hostel, food_months, transport, total_cost)
+
+conn.commit()
+print("Saved to database successfully!")
+
+save_bill(name, course_fee, analytics_fee, hostel_fee, food_fee, transport_fee, total_cost) 
